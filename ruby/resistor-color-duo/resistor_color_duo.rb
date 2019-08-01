082bb890d1f2204c29f0c2c29e43cc0b512a64a5
Miguel Machado <mantonio@student.dei.uc.pt>
2019-08-01 11:02:06 +0100

    Final touches to complete resistor-color-duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,24 +1,18 @@
 class ResistorColorDuo
-  BAND_COLORS = {
-      black: 0,
-      brown: 1,
-      red: 2,
-      orange: 3,
-      yellow: 4,
-      green: 5,
-      blue: 6,
-      violet: 7,
-      grey: 8,
-      white: 9
+  COLOR_TO_VALUE = {
+      "black" => 0,
+      "brown" => 1,
+      "red" => 2,
+      "orange" => 3,
+      "yellow" => 4,
+      "green" => 5,
+      "blue" => 6,
+      "violet" => 7,
+      "grey" => 8,
+      "white" => 9
   }
 
   def self.value(colors)
-    values = []
-
-    colors.each do |color|
-      values.push(BAND_COLORS[color.to_sym])
-    end
-
-    values.join.to_i
+    colors.map(&COLOR_TO_VALUE).join.to_i
   end
 end
