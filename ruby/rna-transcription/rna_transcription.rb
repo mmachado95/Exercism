bc8032417a49299759136216c664b32f5542f440
Miguel Machado <mantonio@student.dei.uc.pt>
2019-08-01 11:02:37 +0100

    Improves ruby rna-transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,31 +1,12 @@
 class Complement
+  RNA_COMPLEMENT = {
+    "C" => "G",
+    "G" => "C",
+    "T" => "A",
+    "A" => "U",
+  }
+
   def self.of_dna(nucleotides)
-    rna_nucleotides = ""
-
-    nucleotides.each_char {|nucleotide| rna_nucleotides << complement(nucleotide) }
-
-    rna_nucleotides
-  end
-
-  def self.complement(nucleotide)
-    # Returns the RNA complement of the DNA nucleotide.
-    # A DNA nucleotide is translated to RNA following these rules:
-    #   G -> C
-    #   C -> G
-    #   T -> A
-    #   A -> U
-
-    case nucleotide
-    when "C"
-      "G"
-    when "G"
-      "C"
-    when "T"
-      "A"
-    when "A"
-      "U"
-    else
-      ""
-    end
+    nucleotides.each_char.map {|nucleotide| RNA_COMPLEMENT[nucleotide] }.join
   end
 end
