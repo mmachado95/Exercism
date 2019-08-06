32412e3da7d50186733ddd17ebb3d0e852169de7
Miguel Machado <mantonio@student.dei.uc.pt>
2019-08-06 15:15:32 +0100

    Completes space-age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,14 +1,14 @@
 class SpaceAge
   EARTH_ORBITAL_PERIOD = 31_557_600.0
   ORBITAL_YEARS = {
-    earth: EARTH_ORBITAL_PERIOD,
-    mercury: EARTH_ORBITAL_PERIOD * 0.2408467,
-    venus: EARTH_ORBITAL_PERIOD * 0.61519726,
-    mars: EARTH_ORBITAL_PERIOD * 1.8808158,
-    jupiter: EARTH_ORBITAL_PERIOD * 11.862615,
-    saturn: EARTH_ORBITAL_PERIOD * 29.447498,
-    uranus: EARTH_ORBITAL_PERIOD * 84.016846,
-    neptune: EARTH_ORBITAL_PERIOD * 164.79132
+    earth: 1
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   }
 
   def initialize(age)
@@ -17,7 +17,7 @@ class SpaceAge
 
   ORBITAL_YEARS.each do |planet, orbital_year|
     define_method("on_#{planet}") do
-      (@age / orbital_year).round(2)
+      (@age / (EARTH_ORBITAL_PERIOD * orbital_year)).round(2)
     end
   end
 end
