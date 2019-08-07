a023cb2b3a7842d705b1e16bd03007acbf31a8c9
Miguel Machado <mantonio@student.dei.uc.pt>
2019-08-07 15:52:04 +0100

    Finishes space age from ruby track

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,7 +1,5 @@
 class Year
   def self.leap?(year)
-    return false unless year % 4 == 0
-    return true unless year % 100 == 0
-    year % 400 == 0
+    year % 4 == 0  && year % 100 != 0 || year % 400 == 0
   end
 end
